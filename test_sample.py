f498aa27f5b6283a4e377cd66b180d2631b69602
chirag bhatt <149137442+github-chiragbhatt@users.noreply.github.com>
2025-07-06 23:05:18 +0530

    added : pytest and implemented test file as test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,18 @@
+# test_sample.py
+import pytest
+from main import *
+# def add(a, b):
+#     return a + b
+
+
+def test_add():
+        assert add_two_values(2, 3) == 5  # Test passes
+        assert add_two_values(1, 1) == 2  # Test passes
+        # assert add_two_values(0, 0) == 1  # Test fails
+        assert add_two_values(0, 0) == 0  # Test passes
+
+def test_sub():
+        assert subs_two_values(5, 3) == 2
+
+def test_div():
+        assert div_two_values(30, 10) == 3 
