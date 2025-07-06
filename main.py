0abda1fa95f0e9e95b20da8470e5637934e647be
chirag bhatt <149137442+github-chiragbhatt@users.noreply.github.com>
2025-07-06 22:48:32 +0530

    added : custom functions in file main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import numpy as np, pandas as pd, matplotlib.pyplot as plt, seaborn as sns
 
+# Custome Calculator Functions
+
+def add_two_values(value1, value2):
+        x = value1
+        y = value2
+        return x + y
+
+
+def subs_two_values(value1, value2):
+        x = value1
+        y = value2
+        return x - y
+
+
+def div_two_values(value1, value2):
+        x = value1
+        y = value2
+        return x / y
+
+
